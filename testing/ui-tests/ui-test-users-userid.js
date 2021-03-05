b3220043338454268007c26c0644e73db2200343
OyunominJuno <oyunominmun@gmail.com>
2021-03-04 17:08:58 -0800

    commented out the screenshot feature for now

--- testing/ui-tests/ui-test-users-userid.js
+++ testing/ui-tests/ui-test-users-userid.js
@@ -5,6 +5,6 @@ const puppeteer = require('puppeteer');
       await page.goto('https://nscstrdevusw2tuecommon.z5.web.core.windows.net/users/46');
       let date = new Date();
       let stringDate = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
-      await page.screenshot({path: '../results/ad440-users-46-screenshot' + stringDate + '.png'});
+      //await page.screenshot({path: '../results/ad440-users-46-screenshot' + stringDate + '.png'});
       await browser.close();
     })();
